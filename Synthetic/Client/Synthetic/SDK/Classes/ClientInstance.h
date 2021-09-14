00b7f6ae57f95b87d6904140e900cd1aa10dea6f
EchoHackCmd <EchoHackCmd@gmail.com>
2021-09-14 18:54:37 +0100

    Update ClientInstance.h

--- Synthetic/Client/Synthetic/SDK/Classes/ClientInstance.h
+++ Synthetic/Client/Synthetic/SDK/Classes/ClientInstance.h
@@ -1,9 +1,15 @@
 #ifndef CLIENT_SYNTHETIC_SDK_CLASSES_CLIENTINSTANCE
 #define CLIENT_SYNTHETIC_SDK_CLASSES_CLIENTINSTANCE
 
+#include "../../../Mem/Mem.h"
+#include "../../../Math/Math.h"
+#include "../../../Utils/Utils.h"
+
+#include "Player.h"
+
 class ClientInstance {
 public:
-    //
+    auto getPlayer() -> Player*;
 };
 
 #endif /* CLIENT_SYNTHETIC_SDK_CLASSES_CLIENTINSTANCE */
