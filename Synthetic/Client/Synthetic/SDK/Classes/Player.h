44595116656d8ecec7746d42f510be1f1d255935
EchoHackCmd <EchoHackCmd@gmail.com>
2021-09-14 18:54:41 +0100

    Create Player.h

---
+++ Synthetic/Client/Synthetic/SDK/Classes/Player.h
@@ -0,0 +1,11 @@
+#ifndef CLIENT_SYNTHETIC_SDK_CLASSES_PLAYER
+#define CLIENT_SYNTHETIC_SDK_CLASSES_PLAYER
+
+#include "Actor.h"
+
+class Player : public Actor {
+public:
+    //
+};
+
+#endif /* CLIENT_SYNTHETIC_SDK_CLASSES_PLAYER */
