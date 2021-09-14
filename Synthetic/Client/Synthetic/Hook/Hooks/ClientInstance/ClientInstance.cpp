fe666c811ccbbd42109c17a87746f33518b15cb1
EchoHackCmd <EchoHackCmd@gmail.com>
2021-09-14 18:13:48 +0100

    Update ClientInstance.cpp

--- Synthetic/Client/Synthetic/Hook/Hooks/ClientInstance/ClientInstance.cpp
+++ Synthetic/Client/Synthetic/Hook/Hooks/ClientInstance/ClientInstance.cpp
@@ -1,5 +1,9 @@
 #include "ClientInstance.h"
 
+#include "../../../Manager/Manager.h"
+#include "../../../Category/Category.h"
+#include "../../../Module/Module.h"
+
 Hook_ClientInstance::Hook_ClientInstance(Manager* manager){
     this->setManager(manager);
     this->init();
@@ -8,25 +12,16 @@ Hook_ClientInstance::Hook_ClientInstance(Manager* manager){
 typedef void(__thiscall* tick)(void*, ClientInstance*);
 tick _tick;
 
-Manager* i_manager = nullptr;
-
 void callback(void* a1, ClientInstance* instance){
     Minecraft::setClientInstance(instance);
-
-    if(i_manager != nullptr){
-        //
-    };
-
     _tick(a1, instance);
 };
 
 void Hook_ClientInstance::init(){
-    auto sig = Mem::findSig("48 8B 81 ? ? ? ? C3 CC CC CC CC CC CC CC CC 40 55");
+    auto sig = Mem::findSig("48 8B 89 ? ? ? ? 48 85 C9 ? ? 33 C0 48 8B 5C 24 ?");
     
     if(!sig)
         return;
-    
-    i_manager = this->getManager();
     
     if(MH_CreateHook((void*)sig, &callback, reinterpret_cast<LPVOID*>(&_tick)) == MH_OK){
         Utils::debugLogF("Successfully created hook for ClientInstance tick!");
