cdbc40e2a6354fa0eb0638ed841008ee6f4978d4
EchoHackCmd <EchoHackCmd@gmail.com>
2021-09-17 22:28:47 +0100

    Update Mem.h

--- Synthetic/Client/Mem/Mem.h
+++ Synthetic/Client/Mem/Mem.h
@@ -11,9 +11,13 @@
 #define GET_BITS( x )    (INRANGE((x&(~0x20)),'A','F') ? ((x&(~0x20)) - 'A' + 0xa) : (INRANGE(x,'0','9') ? x - '0' : 0))
 
 class Mem {
+private:
+    static HMODULE hMod;
 public:
     static auto findSig(const char*) -> uintptr_t;
     static auto findMultiLvlPtr(uintptr_t, std::vector<unsigned int>) -> uintptr_t;
+    static auto setHModule(HMODULE) -> void;
+    static auto getHModule() -> HMODULE;
 };
 
 #endif /* CLIENT_MEM_MEM */
